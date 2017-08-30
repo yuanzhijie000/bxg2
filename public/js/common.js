eee85b15f26bb24e4b4669abcf4f88f2bd6ddf65
yuanzhijie000 <651273104@qq.com>
2017-08-30 13:08:44 +0800

    退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -6,3 +6,17 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	// 退出
+	$('#logoutBtn').click(function(){
+		$.ajax({
+			url: 'api/logout',
+			type: 'POST',
+			dataType: 'json',
+			success:function(data){
+				if (data.code==200) {
+					location.href='/main/login';
+				}
+			}
+		});
+	});
